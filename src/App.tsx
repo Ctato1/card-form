3077da3aa5736580cc3044f0dbe00306696b4581
Ctato1 <dznelashvilitato@gmail.com>
2024-03-29 18:55:59 +0400

    fix section styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,22 @@ function App() {
   
   return (
     <div className="mx-auto  rounded-md md:flex">
-      <section className="bg-gradient-to-tr from-[#21092F] via-purple-900 to-purple-800 px-4 h-[250px] w-full mx-auto
+      <section className="
+      bg-resp
+      md:bg-desk
+      w-full mx-auto
       md:w-[400px] md:mx-0 md:h-screen md:flex md:flex-col-reverse md:justify-center md:space-y-4
-      md:relative md:bg-gradient-to-tr
+      md:relative 
       flex justify-between flex-col
       ">
-        <div className=" pt-[60px] pl-[3vw] self-end  md:absolute md:bottom-[30vh] md:left-[15vw]">
+        <div className=" pt-[60px] pl-[3vw] self-end  md:absolute md:bottom-[30vh] md:left-[15.5vw]">
           <BackCard />
         </div>
         <div className=" z-10 mt-[-75px] ml-4 md:absolute md:top-[20vh] md:left-[10vw]">
           <FrontCard name={name}/>
         </div>
       </section>
-      <main className="w-full  md:mx-auto  md:flex md:justify-end md:items-center">
+      <main className="  md:mx-auto  md:flex md:justify-end md:items-center">
         <Form setName={setName}/>
       </main>
     </div>
