3b7edf263340af1fc744e8c0e69cf0920ed73083
Ctato1 <dznelashvilitato@gmail.com>
2024-03-30 15:33:37 +0400

    add html label elements

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -28,8 +28,9 @@ const Form = ({ setName, setNumber, setCount, count, number }: FormProps) => {
   return (
     <form className="mt-28 px-[24px] md:mt-0 md:w-[60%] md:float-right xl:w-[380px] xl:float-none">
       <div className="mt-[20px] flex flex-col">
-        <label className="label">Cardholder Name</label>
+        <label className="label" htmlFor="name">Cardholder Name</label>
         <input
+        id="name"
           type="text"
           placeholder="e.g. Jane Appleseed"
           className="input"
@@ -38,8 +39,9 @@ const Form = ({ setName, setNumber, setCount, count, number }: FormProps) => {
       </div>
 
       <div className="mt-[20px] flex flex-col">
-        <label className="label">Card Number</label>
+        <label className="label" htmlFor="number">Card Number</label>
         <input
+        id="number"
           type="text"
           placeholder="e.g. Jane Appleseed"
           className="input"
@@ -55,13 +57,13 @@ const Form = ({ setName, setNumber, setCount, count, number }: FormProps) => {
 
       <div className="mt-[20px] flex flex-col">
         <div className="flex gap-3">
-          <label className="label">Exp. Date (MM/YY)</label>
-          <label className="label">CVC</label>
+          <label className="label" htmlFor="date">Exp. Date (MM/YY)</label>
+          <label className="label" htmlFor="cvc">CVC</label>
         </div>
         <ul className="flex justify-between gap-2 w-full">
           <li className="">
             {" "}
-            <input type="text" placeholder="MM" className="input w-[90%]" />
+            <input type="text" id="date" placeholder="MM" className="input w-[90%]" />
           </li>
           <li>
             {" "}
@@ -71,6 +73,7 @@ const Form = ({ setName, setNumber, setCount, count, number }: FormProps) => {
             {" "}
             <input
               type="text"
+              id="cvc"
               placeholder="e.g. 123"
               className="input w-[40vw] md:w-[10vw] xl:w-full"
             />
